352b79e25123f19bbe567ab6d6a4e1b21eb209c8
Tuan LE CONG <tlcong@linagora.com>
2022-05-27 07:16:45 +0700

    Allow to pass directory path

--- file-server.go
+++ file-server.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+  "fmt"
   "log"
   "net/http"
+  flag "github.com/ogier/pflag"
+)
+
+var (
+  path string
 )
 
 func main() {
+  flag.Parse()
+  fmt.Printf("Directory path: %s\n", path)
   // Simple static webserver:
-  log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
+  fmt.Printf("File server is runing at http://localhost:8080\n")
+  log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir(path))))
+}
+
+func init() {
+  flag.StringVarP(&path, "path", "p", "", "Directory path")
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module file-server
+
+go 1.18
+
+require github.com/ogier/pflag v0.0.1 // indirect
